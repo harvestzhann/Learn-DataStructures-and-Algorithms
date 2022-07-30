55b9b4b05bbf1ab94e9c606895784effd03efaea
harvestzhann <harvestzhann@gmail.com>
2022-07-30 23:53:39 +0700

    learn exponential search algorithm

---
+++ Algorithms/Searchings/exponential_search.py
@@ -0,0 +1,12 @@
+def exponential_search(arr: list, find: int):
+   pass
+
+
+def main():
+   arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+   find = 11
+   print(exponential_search(arr, find))
+
+
+if __name__ == '__main__':
+   main()
