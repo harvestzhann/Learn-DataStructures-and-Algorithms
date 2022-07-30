1d657d4650fafe4047ed33fcee6e26627c4a3837
harvestzhann <harvestzhann@gmail.com>
2022-07-30 23:15:09 +0700

    done revising topics

--- Algorithms/Searchings/revise_1.py
+++ Algorithms/Searchings/revise_1.py
@@ -1,11 +1,14 @@
+# Jump Search
+
+
 import math
 
 
-def jump_search(arr:list, find:int):
+def jump_search(arr: list, find: int) -> int:
    length = len(arr)
-   step = int(math.sqrt(length))
    index = 0
-   
+   step = int(math.sqrt(length))
+
    while arr[min(step, length)-1] < find:
       index = step
       step += int(math.sqrt(length))
@@ -14,16 +17,15 @@ def jump_search(arr:list, find:int):
    
    while arr[index] < find:
       index += 1
-      if index == min(step, length):
-         return -1
    
    if arr[index] == find:
       return index
    return -1
+   
 
 
 def main():
-   arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 25, 30]
+   arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 , 22, 24, 25, 30]
    find = 30
    print(jump_search(arr, find))
 
