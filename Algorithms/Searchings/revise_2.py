1ae957554bc1fc2ebf886dae90ebebcdaf317ddd
harvestzhann <harvestzhann@gmail.com>
2022-08-01 21:36:44 +0700

    revise exponential search algorithm

--- Algorithms/Searchings/revise_2.py
+++ Algorithms/Searchings/revise_2.py
@@ -1,23 +1,34 @@
-# Binary Search with Recursion Algorithm
+# Exponential Search Algorithm
 
 
-def binary_search_recursion(arr: list, find: int) -> float:
+def binary_search_recursion(arr: list, start: int, end: int, find: int) -> int:
    if len(arr) > 0:
-      middle = len(arr) // 2
+      middle = (start + end) // 2
       if arr[middle] == find:
-         return True
+         return middle
       else:
          if find < arr[middle]:
-            return binary_search_recursion(arr[:middle], find)
+            return binary_search_recursion(arr, start, arr[:middle], find)
          else:
-            return binary_search_recursion(arr[middle + 1:], find)
-   return False
+            return binary_search_recursion(arr, arr[:middle], end, find)
+   return -1
+
+
+def exponential_search(arr: list, find: int) -> int:
+   if arr[0] == find:
+      return 0
+   
+   index = 1
+   while arr[min(index, len(arr)-1)] < find:
+      index *= 2
+   
+   return binary_search_recursion(arr, index//2, min(index, len(arr)-1), find)
 
 
 def main():
    arr: list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 , 22, 24, 25, 30]
    find: int = 1
-   print(binary_search_recursion(arr, find))
+   print(exponential_search(arr, find))
 
 
 if __name__ == '__main__':
