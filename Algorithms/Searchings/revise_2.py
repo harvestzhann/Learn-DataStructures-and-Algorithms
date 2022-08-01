079ff66b4ad220174f4e4d251307a7bf24d0d73e
harvestzhann <harvestzhann@gmail.com>
2022-08-01 21:49:30 +0700

    correction for exponential search algorithm (revise 2)

--- Algorithms/Searchings/revise_2.py
+++ Algorithms/Searchings/revise_2.py
@@ -1,17 +1,18 @@
 # Exponential Search Algorithm
 
 
-def binary_search_recursion(arr: list, start: int, end: int, find: int) -> int:
-   if len(arr) > 0:
+def binary_search(arr: list, start: int, end: int, find: int) -> int:
+   if start != end:
       middle = (start + end) // 2
       if arr[middle] == find:
          return middle
       else:
-         if find < arr[middle]:
-            return binary_search_recursion(arr, start, arr[:middle], find)
+         if arr[middle] > find:
+            return binary_search(arr, start, middle-1, find)
          else:
-            return binary_search_recursion(arr, arr[:middle], end, find)
-   return -1
+            return binary_search(arr, middle+1, end, find)
+   else:
+      return -1
 
 
 def exponential_search(arr: list, find: int) -> int:
@@ -19,15 +20,15 @@ def exponential_search(arr: list, find: int) -> int:
       return 0
    
    index = 1
-   while arr[min(index, len(arr)-1)] < find:
+   while index < len(arr) and arr[index] <= find:
       index *= 2
    
-   return binary_search_recursion(arr, index//2, min(index, len(arr)-1), find)
+   return binary_search(arr, index//2, min(index, len(arr)-1), find)
 
 
 def main():
    arr: list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 , 22, 24, 25, 30]
-   find: int = 1
+   find: int = 11
    print(exponential_search(arr, find))
 
 
