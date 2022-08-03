e494f9b16883923443833688b516b39f7cc33bf7
harvestzhann <harvestzhann@gmail.com>
2022-08-03 20:56:03 +0700

    update interpolation_searph.py

--- Algorithms/Searchings/interpolation_search.py
+++ Algorithms/Searchings/interpolation_search.py
@@ -16,11 +16,12 @@ def interpolation_search(arr: list, find: int) -> int:
             start = pointer + 1
          else:
             end = pointer - 1
+   return -1
 
 
 def main():
    arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 , 22, 24, 25, 30]
-   find = 20
+   find = 1
    print(interpolation_search(arr, find))
 
 
