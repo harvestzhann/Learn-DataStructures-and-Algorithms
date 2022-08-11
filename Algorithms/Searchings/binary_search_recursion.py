a1b95f7d951c343931da251a84462ddd7d643d98
harvestzhann <harvestzhann@gmail.com>
2022-08-11 19:18:25 +0700

    output binary_search_recursion changed to int

--- Algorithms/Searchings/binary_search_recursion.py
+++ Algorithms/Searchings/binary_search_recursion.py
@@ -1,17 +1,17 @@
-def binary_search_recursion(arr:list, find:int) -> float:
+def binary_search_recursion(arr: list, start: int, end: int, find: int) -> int:
    # check if arr is sorted
    if sorted(arr) == arr:
-      if len(arr) > 0:
-         middle = len(arr) // 2
+      if start != end:
+         middle = (start + end) // 2
          if arr[middle] == find:
-            return True
+            return middle
          else:
             if find < arr[middle]:
-               return binary_search_recursion(arr[:middle], find)
+               return binary_search_recursion(arr, start, middle-1, find)
             else:
-               return binary_search_recursion(arr[middle+1:], find)
+               return binary_search_recursion(arr, middle+1, end, find)
       else:
-         return False
+         return -1
    
    raise ValueError("'arr' must be sorted")
 
@@ -19,7 +19,7 @@ def binary_search_recursion(arr:list, find:int) -> float:
 def main():
    arr = [0, 2, 4, 6, 8, 10, 12, 14]
    find = 12
-   print(binary_search_recursion(arr, find))
+   print(binary_search_recursion(arr, 0, len(arr)-1, find))
 
 
 if __name__ == '__main__':
